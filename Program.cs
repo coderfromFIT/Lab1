083f71f9ea1e8ace786fa5f356313954ace4587d
{{coderfromFIT}} <{{narobotu45@gmail.com}}>
2023-09-08 20:26:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Натисніть будь-яку клавішу, щоб вийти...");
+            Console.ReadKey(); // Чекаємо на натискання клавіші
+        }
+    }
+}
